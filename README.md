11fe39127591a5616482f703e34e4173ff097c4c
Rafael Santos Freire <contato@rafascripts.site>
2021-04-13 22:53:30 -0300

    day2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Segundo Dia #100DaysOfCode
+<h1>Segundo Dia #100DaysOfCode</h1>
+
+<p>Segundo dia do Desafio, este é um resumo de como é feita a declaração de <br>
+variáveis no JavaScript, desde uma forma mais antiga e menos utilizada até a mais atual. </p>
---
+++ script.js
@@ -0,0 +1,55 @@
+/**
+ * Variáveis como declaralas
+ * 
+ * São uma forma de salvar valores em um espaço determinado da memória
+ * 
+ * No JavaScript e Possivel Declaralas de 3 formas:
+ * 
+ * let
+ * 
+ * var
+ * 
+ * const
+ * 
+ * Sendo o (var) o mais antigo e menos recomendado por sua compatibilidade.
+ * 
+ * os mais utilizados hoje principalmente por suas frameworks são o:
+ * 
+ * let e const
+ * 
+ * sendo (const) contendo uma propiedade especial de que seu valor depois de declarado
+ * não pode ser alterado (Cont deriva de Constant(constante)).
+ * 
+ * Exemplos:
+ * 
+ */
+
+let name = "rafael";
+
+console.log(name);
+
+const nome = 1;
+
+console.log(nome);
+
+var Nome = true;
+
+console.log(Nome);
+
+/*************************************************************************************/
+console.log("---------------------------------------------------------------");
+/*************************************************************************************/
+
+//let name = 1;
+
+//console.log(name == name && "1" == 1);
+
+var Nome = true;
+
+console.log(!(Nome));
+
+//const nome = 2;
+ 
+//console.log(nome);
+
+
